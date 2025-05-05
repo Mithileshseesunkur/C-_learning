c2a8ca79637fc18a8c1658a2542881c87956c652
Mithileshseesunkur <zavallingum@gmail.com>
2025-05-05 22:05:30 +0200

    learning x++ etc

--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
 
+/*int main()
+{
+    int fileSize = 100;
+    double sales= 9.99;
+    std::cout<< fileSize << std::endl;
+    std::cout << "Hello World!" << std::endl;
+
+    int x,y;
+    x=10;
+    y=3;
+
+    double z=x+y;
+    std::cout << z << std::endl;
+
+    y=x++; // y=10, x=11
+    z=++x; // x=12, z=12
+    std::cout << y << std::endl; // 10
+    return 0;
+}*/
+
 int main()
 {
-    std::cout << "Hello World!";
+    
     return 0;
 }
